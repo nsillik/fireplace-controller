a6e26874702f24388333f82f58b6a5b7ca8cdb18
Nick Sillik <nick@sillik.org>
2023-02-04 22:08:45 -0500

    get rid of useless led blink

--- flicker/src/main.c
+++ flicker/src/main.c
@@ -9,17 +9,6 @@
 
 #define LED_PIN 22
 
-void led_blink(void *pvParams) {
-    gpio_pad_select_gpio(LED_PIN);
-    gpio_set_direction (LED_PIN,GPIO_MODE_OUTPUT);
-    while (1) {
-        gpio_set_level(LED_PIN,0);
-        vTaskDelay(900/portTICK_RATE_MS);
-        gpio_set_level(LED_PIN,1);
-        vTaskDelay(100/portTICK_RATE_MS);
-    }
-}
-
 void app_main() {
     //Initialize NVS
     esp_err_t ret = nvs_flash_init();
@@ -33,6 +22,5 @@ void app_main() {
     ESP_LOGI("FLICKER", "ESP_WIFI_MODE_STA");
     wifi_init_sta();
     xTaskCreate(&udp_server_task, "udp_server", 4096, (void*)AF_INET, 5, NULL);
-    xTaskCreate(&led_blink,"LED_BLINK",4096,NULL,5,NULL);
     xTaskCreate(&statusLoop,"STATUS_LOOP",4096,NULL,5,NULL);
 }
